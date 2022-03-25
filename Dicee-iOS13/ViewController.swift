52881b021f3a36d15de1e7c9c834a8542979810c
Shahzad Umar Baig <ShahzadUBaig@gmail.com>
2022-03-25 16:45:45 +0500

    dice app complete

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -1,20 +1,22 @@
-//
-//  ViewController.swift
-//  Dicee-iOS13
-//
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
-
 import UIKit
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+   
+    let allDiceFaces = ["DiceOne","DiceTwo","DiceThree", "DiceFour", "DiceFive", "DiceSix"];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageViewOne.image = UIImage(named: allDiceFaces.randomElement()!)
+        diceImageViewTwo.image = UIImage(named: allDiceFaces.randomElement()!)
+    }
+    
 }
 
